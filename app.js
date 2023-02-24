b5cd8f11b110ab02ae3c33fd151e991db8ccccff
Romane Orlando Robb <mail@romanerobb.com>
2023-02-24 15:31:45 -0500

    Updated heroku files.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -17,19 +18,61 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
 
-    let firstName = req.body.fName;
-    let lastName = req.body.lName;
-    let emailAddress = req.body.emailAddress;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
 
-    console.log(firstName, lastName, emailAddress);
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us14.api.mailchimp.com/3.0/lists/c400c0053d";
+
+    const options = {
+        method: "POST",
+        auth: "romanerobb:97564b4c5bedad349a6f3d386b1028c8-us14"
+    }
+
+   const request = https.request(url, options, function(response){
+
+    if (response.statusCode == 200) {
+        res.sendFile(__dirname + "/success.html");
+    } else {
+        res.sendFile(__dirname + "/failure.html");
+    };
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+
+    });
+
+    request.write(jsonData);
+    request.end();
+
 });
 
-app.listen(3000, function(){
+app.post("/failure", function(req, res){
+    res.redirect("/")
+});
+
+app.listen(process.env.PORT || 3000, function(){
     console.log("Robb Server is running on port 3000");
 });
 
 //API Key
-// f688e6be55ddd673f2fd79f276c8a322-us14
+// 97564b4c5bedad349a6f3d386b1028c8-us14
 
 //List Id
 // c400c0053d
