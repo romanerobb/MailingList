59874c2d78cc40854ce19f56af2664aa7b5d6916
Romane Orlando Robb <mail@romanerobb.com>
2023-02-24 11:28:05 -0500

    Updated keys.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// The following block of code are standard boilerplate code for ALL express projects.
+// The following block of code below are standard boilerplate code for ALL express projects.
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-// The following block of code are standard boilerplate code for ALL express projects.
+// The following block of code above are standard boilerplate code for ALL express projects.
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
@@ -17,9 +17,19 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
 
+    let firstName = req.body.fName;
+    let lastName = req.body.lName;
+    let emailAddress = req.body.emailAddress;
 
-})
+    console.log(firstName, lastName, emailAddress);
+});
 
 app.listen(3000, function(){
     console.log("Robb Server is running on port 3000");
 });
+
+//API Key
+// f688e6be55ddd673f2fd79f276c8a322-us14
+
+//List Id
+// c400c0053d
